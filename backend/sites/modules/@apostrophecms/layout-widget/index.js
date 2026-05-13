a977b305b32b68c3b2dd954ef660072d71bbd436
Thomas Boutell <tom@apostrophecms.com>
2026-05-13 14:24:32 -0400

    fix weird pinned deps, add layout gap

--- backend/sites/modules/@apostrophecms/layout-widget/index.js
+++ backend/sites/modules/@apostrophecms/layout-widget/index.js
@@ -3,5 +3,18 @@ export default {
     label: 'project:layout',
     description: 'project:layoutDescription',
     previewImage: 'svg'
+  },
+  styles: {
+    add: {
+      gap: {
+        label: 'apostrophe:styleLayoutGap',
+        type: 'range',
+        min: 0,
+        def: 24,
+        max: 64,
+        unit: 'px',
+        property: 'gap'
+      }
+    }
   }
 };
